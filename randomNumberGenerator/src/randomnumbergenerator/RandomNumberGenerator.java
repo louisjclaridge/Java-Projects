67afbb12c15024b454fa3c699beb233457d76f1c
Louis Claridge <33028861+louisjclaridge@users.noreply.github.com>
2018-09-28 13:09:44 +0100

    finished random number generator

--- randomNumberGenerator/src/randomnumbergenerator/RandomNumberGenerator.java
+++ randomNumberGenerator/src/randomnumbergenerator/RandomNumberGenerator.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package randomnumbergenerator;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +16,27 @@ public class RandomNumberGenerator {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+       randomNumberGen();
     }
     
+    public static void randomNumberGen()
+    {
+        //Initalises the scanner for use 
+        Scanner userInput = new Scanner(System.in);
+        //Initialises the random for use
+        Random randomNumber = new Random();
+        
+        
+        //Getting values from the user
+        System.out.print("Enter minimum value" + System.lineSeparator());
+        int minValue = Integer.parseInt(userInput.next());
+        System.out.print("Enter maximum value" + System.lineSeparator());
+        int maxValue = Integer.parseInt(userInput.next());
+        
+        int Ouput = randomNumber.nextInt(maxValue) + minValue;
+        
+        System.out.print("Your random number is: " + Ouput + System.lineSeparator());
+    }
 }
